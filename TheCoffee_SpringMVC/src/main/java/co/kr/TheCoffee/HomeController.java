8a5488554a6753b3d529f8042baa69c757b72e5b
Seungho <83917282+sshhhGit@users.noreply.github.com>
2021-10-14 23:54:39 +0900

    Add

--- TheCoffee_SpringMVC/src/main/java/co/kr/TheCoffee/HomeController.java
+++ TheCoffee_SpringMVC/src/main/java/co/kr/TheCoffee/HomeController.java
@@ -33,8 +33,8 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		//return "home";
-		return ".main.layout";
+		return "home";
+		//return ".main.layout";
 	}
 	
 }
